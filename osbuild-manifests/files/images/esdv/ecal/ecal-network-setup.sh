ba3ebd8f341f608e65be01ddc12378ed8d0bf000
Leonardo Rossetti <lrossett@redhat.com>
2022-12-22 11:56:16 -0300

    removing hostNetwork
    Signed-off-by: Leonardo Rossetti <lrossett@redhat.com>

--- osbuild-manifests/files/images/esdv/ecal/ecal-network-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ifconfig lo multicast
-route add -net 239.0.0.0 netmask 255.255.255.0 dev lo metric 1000
-
-ifconfig enp0s2 multicast
-route add -net 239.0.0.0 netmask 255.255.255.0 dev enp0s2 metric 1
