23150386a5124e52a71b203079916d5e5068afbb
Eric Curtin <ecurtin@redhat.com>
2024-03-01 13:11:12 +0000

    Automotive Image Builder script
    This simplifies how we build images, this command can be run:
    
    sudo ./auto-image-builder.sh cs9-qemu-minimal-ostree.aarch64.qcow2
    
    No need to worry about installing dependencies (except podman), it just
    works.
    
    Signed-off-by: Eric Curtin <ecurtin@redhat.com>

---
+++ auto-image-builder.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 0
+fi
+
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+cd "$SCRIPTPATH"
+
+sudo podman run -v "$PWD":"$PWD" --rm --privileged --pull=newer --security-opt label=type:unconfined_t quay.io/centos-sig-automotive/automotive-osbuild /bin/bash -c "cd $PWD/osbuild-manifests/; make $@"
+
