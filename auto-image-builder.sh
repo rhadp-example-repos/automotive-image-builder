eadbeec9614d8b1fa9d3b7642c6e6ca0a0cb655b
Eric Curtin <ecurtin@redhat.com>
2024-03-25 11:46:01 +0000

    auto-image-builder.sh add nopull and usage information
    - Added a `usage` function to display script usage and options.
    - Added options `-h` or `--help` to display usage.
    - Added option `-n` or `--nopull` to skip pulling new images.
    
    Signed-off-by: Eric Curtin <ecurtin@redhat.com>

--- auto-image-builder.sh
+++ auto-image-builder.sh
@@ -2,6 +2,33 @@
 
 set -e
 
+usage() {
+    echo "Usage:"
+    echo "  auto-image-builder.sh [OPTION...] MAKE_ARGS"
+    echo
+    echo "Options:"
+    echo "  -h,--help                - Display usage"
+    echo "  -n,--nopull              - Don't attempt to pull new image"
+    echo
+}
+
+PULL_ARG="--pull=newer"
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -h|--help)
+      usage
+      exit 0
+      ;;
+    -n|--nopull)
+      PULL_ARG=
+      shift 1
+      ;;
+    *)
+      break;
+      ;;
+  esac
+done
+
 if [ "$EUID" -ne 0 ]; then
   echo "Please run as root"
   exit 0
@@ -12,5 +39,5 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 
 cd "$SCRIPTPATH"
 
-podman run -v "$PWD":"$PWD" --rm --privileged --pull=newer --security-opt label=type:unconfined_t quay.io/centos-sig-automotive/automotive-osbuild /bin/bash -c "cd $PWD/osbuild-manifests/; make $@"
+podman run -v "$PWD":"$PWD" --rm --privileged $PULL_ARG --security-opt label=type:unconfined_t quay.io/centos-sig-automotive/automotive-osbuild /bin/bash -c "cd $PWD/osbuild-manifests/; make $@"
 
