effd7c5fa76ce24434678017a8a94633e0cc83ec
Alexander Larsson <alexl@redhat.com>
2024-10-24 10:30:31 +0200

    Restructure include handling of snippets
    This changes the structure of includes of the various yaml snippets
    with the goal of simplifying maintainance of the various a-i-b files
    and making writing manfests more powerful and understandable.
    
    The order of evaluation of the various mpp files when handling
    includes is a bit weird. First the main yaml has mpp-vars evalutated,
    and then the references includes are loaded and evaluated in include
    order. This also applies recursively, further complicating things.
    
    This makes reasoning about evalutation order hard, and additionaly:
    
    1) The manifest file cannot access variables that are defined by
       default or for the chosed arch/target/distro. This is problematic
       for e.g. the qa manifest that want to e.g customize the external /var
       config based on the value of use_aboot.
    
    2) The file that sets the defaults has to manually check for each
       default value if the variable was already set by the user before
       setting it. This leads to some very ugly and hard to maintain code.
    
    The new model is to always pass the "main.ipp.yml" file to
    osbuild-mpp.  This then includes the various files (including the
    end-user manifest) in a linear order, making the evaluation order
    of things very clear, and allowing the end-user manifest to access
    the previously set variables.
    
    The new order for variable expansion is:
     * Compute defaults
       * pure default values
       * distro defaults
       * target defaults
       * arch defaults
       * computed defaults (depends on distro/target/arch)
     * Manifest variables
     * Computed variables, as needed for the pipelines
    
    Also, the files with pipelines have been split up and
    ordeder in one place such that the order is right allowing
    later pipelines to refer to earlier ones.
    
    To get the order of evaluation of the the "name" variable right we
    have to do some custom work. This now allows us to pre-initialize it
    to the manifest basename.
    
    This looks like a large change, but I ran all the compose-tests and
    the produced composed json files were identical to the previous ones
    (other that the timestamps, etc).

--- aib/tests/manifest_test.py
+++ aib/tests/manifest_test.py
@@ -19,7 +19,6 @@ from aib.main import rewrite_manifest
          ]},
         {"name": "qm_rootfs"}
     ]}, [
-        {'mpp-import-pipelines': {'path': 'include/build.ipp.yml'}},
         {"name": "rootfs",
          "stages": [
              {'mpp-eval': 'kernel_cmdline_stage'},
@@ -33,7 +32,6 @@ from aib.main import rewrite_manifest
              },
          ]},
         {"name": "qm_rootfs"},
-        {'mpp-import-pipelines': {'path': 'include/image.ipp.yml'}}
     ], id="rootfs-single-relative"),
     # Multiple input in rootfs all with relative path.
     pytest.param({"pipelines": [
@@ -43,7 +41,7 @@ from aib.main import rewrite_manifest
                                                                 'path': '../files/relative/path/file1.txt'},
                                                   'origin': 'org.osbuild.source',
                                                   'type': 'org.osbuild.files'},
-                        
+
                          'root_extra_content_1': {'mpp-embed': {'id': '6e3b505',
                                                                 'path': 'files/relative/path/file2.txt'},
                                                   'origin': 'org.osbuild.source',
@@ -57,7 +55,6 @@ from aib.main import rewrite_manifest
          ]},
         {"name": "qm_rootfs"}
     ]}, [
-        {'mpp-import-pipelines': {'path': 'include/build.ipp.yml'}},
         {"name": "rootfs",
          "stages": [
              {'mpp-eval': 'kernel_cmdline_stage'},
@@ -77,7 +74,6 @@ from aib.main import rewrite_manifest
              },
          ]},
         {"name": "qm_rootfs"},
-        {'mpp-import-pipelines': {'path': 'include/image.ipp.yml'}}
     ], id="rootfs-multiple-relative"),
     # Multiple input in rootfs only last with relative path.
     pytest.param({"pipelines": [
@@ -101,7 +97,6 @@ from aib.main import rewrite_manifest
          ]},
         {"name": "qm_rootfs"}
     ]}, [
-        {'mpp-import-pipelines': {'path': 'include/build.ipp.yml'}},
         {"name": "rootfs",
          "stages": [
              {'mpp-eval': 'kernel_cmdline_stage'},
@@ -121,7 +116,6 @@ from aib.main import rewrite_manifest
              },
          ]},
         {"name": "qm_rootfs"},
-        {'mpp-import-pipelines': {'path': 'include/image.ipp.yml'}}
     ], id="rootfs-last-relative"),
     # Single input in qm_rootfs with relative path.
     pytest.param({"pipelines": [
@@ -138,7 +132,6 @@ from aib.main import rewrite_manifest
              },
          ]}
     ]}, [
-        {'mpp-import-pipelines': {'path': 'include/build.ipp.yml'}},
         {"name": "rootfs"},
         {"name": "qm_rootfs",
          "stages": [
@@ -150,8 +143,7 @@ from aib.main import rewrite_manifest
               "options": {'paths': [{'from': {'mpp-format-string': "input://qm_extra_content/{embedded['292759f']}"},
                                      'to': 'tree:///etc/destination/file1.conf'}]}
              },
-         ]},
-        {'mpp-import-pipelines': {'path': 'include/image.ipp.yml'}}
+         ]}
     ], id="qm-rootfs-single-relative"),
     # A path not as part of an `mpp-embed` stage shall not be changed.
     # A non-realistic example used for the test purpose.
@@ -168,7 +160,6 @@ from aib.main import rewrite_manifest
              },
          ]}
     ]}, [
-        {'mpp-import-pipelines': {'path': 'include/build.ipp.yml'}},
         {"name": "rootfs"},
         {"name": "qm_rootfs",
          "stages": [
@@ -180,49 +171,22 @@ from aib.main import rewrite_manifest
                                      'to': 'tree:///etc/destination/file1.conf'}]}
              },
          ]},
-        {'mpp-import-pipelines': {'path': 'include/image.ipp.yml'}}
     ], id="qm-rootfs-relative-unchanged"),
-    # Manifest already has a build pipeline imported.
-    pytest.param({"pipelines": [
-        {'mpp-import-pipelines': {'path': 'include/build.ipp.yml'}},
-        {"name": "rootfs"},
-        {"name": "qm_rootfs"}
-    ]}, [
-        {'mpp-import-pipelines': {'path': 'include/build.ipp.yml'}},
-        {"name": "rootfs"},
-        {"name": "qm_rootfs"},
-        {'mpp-import-pipelines': {'path': 'include/image.ipp.yml'}}
-    ], id="already-build-import"),
-    # Manifest already has an image pipeline imported.
-    pytest.param({"pipelines": [
-        {"name": "rootfs"},
-        {"name": "qm_rootfs"},
-        {'mpp-import-pipelines': {'path': 'include/image.ipp.yml'}}
-    ]}, [
-        {'mpp-import-pipelines': {'path': 'include/build.ipp.yml'}},
-        {"name": "rootfs"},
-        {"name": "qm_rootfs"},
-        {'mpp-import-pipelines': {'path': 'include/image.ipp.yml'}}
-    ], id="already-image-import"),
     # The rootfs pipelines has empty stages declared, so appends 'kernel_cmdline_stage'.
     pytest.param({"pipelines": [
         {"name": "rootfs", "stages": []},
         {"name": "qm_rootfs"},
     ]}, [
-        {'mpp-import-pipelines': {'path': 'include/build.ipp.yml'}},
         {"name": "rootfs", "stages": [{"mpp-eval": "kernel_cmdline_stage"}]},
         {"name": "qm_rootfs"},
-        {'mpp-import-pipelines': {'path': 'include/image.ipp.yml'}}
     ], id="adds-kernel-cmdline-stage"),
     # The rootfs pipelines has no stages declared, do nothing with them.
     pytest.param({"pipelines": [
         {"name": "rootfs"},
         {"name": "qm_rootfs"},
     ]}, [
-        {'mpp-import-pipelines': {'path': 'include/build.ipp.yml'}},
         {"name": "rootfs"},
         {"name": "qm_rootfs"},
-        {'mpp-import-pipelines': {'path': 'include/image.ipp.yml'}}
     ], id="omits-kernel-cmdline-stage"),
 ])
 def test_rewrite_manifest(manifest, expected):
