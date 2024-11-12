f28b7f240a9a33f2273c2d8a578b3ebd4aad6623
Alexander Larsson <alexl@redhat.com>
2024-11-12 16:44:16 +0100

    Support simplified manifest format
    This adds support for a new, minimal *.aib.yml manifest format. Here is
    the a small example that does everything default except it adds a vim
    rpm:
    
    ```
    name: simple
    
    content:
      rpms:
        - vim
    ```
    
    The way it works internally is that the manifest basically just
    defines various mpp variables (but in a nicer layouyt) and then we
    expand these in a built-in mpp file.
    
    The code supports both *.mpp.yml files, which work as before, and
    the new one, which is used if the file ends in `.aib.yml`.
    
    The file format is defined using json-schema which makes it very clear
    exactly what is allowed, and anything else will give an error. It also
    allows us to auto-generate the manifest format docs using "make
    generate-manifest-doc".
    
    There are some examples in the "examples" subdir.
    
    NOTE: We also remove the recent support for using "-" to mean
    an empty mpp file that only uses mpp variables. That usecase
    is now taken by the simplified format.

---
+++ aib/simple.py
@@ -0,0 +1,320 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import yaml
+import json
+import re
+
+import jsonschema
+
+from . import exceptions
+
+# Duplicate a dict and drop one key
+def without(d, key):
+    new_d = d.copy()
+    new_d.pop(key)
+    return new_d
+
+def parse_size(s: str):
+    """Parse a size string into a number.
+
+    Supported suffixes: kB, kiB, MB, MiB, GB, GiB, TB, TiB
+    """
+    units = [
+        (r'^\s*(\d+)\s*kB$', 1000, 1),
+        (r'^\s*(\d+)\s*KiB$', 1024, 1),
+        (r'^\s*(\d+)\s*MB$', 1000, 2),
+        (r'^\s*(\d+)\s*MiB$', 1024, 2),
+        (r'^\s*(\d+)\s*GB$', 1000, 3),
+        (r'^\s*(\d+)\s*GiB$', 1024, 3),
+        (r'^\s*(\d+)\s*TB$', 1000, 4),
+        (r'^\s*(\d+)\s*TiB$', 1024, 4),
+        (r'^\s*(\d+)$', 1, 1),
+    ]
+
+    for pat, base, power in units:
+        m = re.fullmatch(pat, s)
+        if m:
+            if isinstance(base, int):
+                return int(m.group(1)) * base ** power
+
+    raise TypeError(f"invalid size value: '{s}'")
+
+# The manifest we use is always empty.mpp.yml, but due to who the
+# osbuil-mpp syntax and evaluation works we need to also generate an
+# extra manifest with the included file content. This is shared betwee
+# the QM and the Rootfs partition
+class ExtraInclude:
+    def __init__(self, basedir):
+        self.basedir = os.path.abspath(basedir)
+        self.content_id = 1
+        self.file_content_inputs = {}
+        self.file_content_paths = []
+
+    def gen_id(self):
+        content_id = self.content_id
+        self.content_id = content_id + 1
+        return content_id
+
+    def gen_file_input(self, content_id, data):
+        mpp_embed = {
+            "id": "image_content_id_" + str(content_id),
+        }
+        if "text" in data:
+            mpp_embed["text"] = data["text"]
+        if "url" in data:
+            mpp_embed["url"] = data["url"]
+        if "source_path" in data:
+            path = data["source_path"]
+            if not os.path.isabs(path):
+                path = os.path.normpath(os.path.join(self.basedir, path))
+            mpp_embed["path"] = path
+        return {
+            "type": "org.osbuild.files",
+            "origin": "org.osbuild.source",
+            "mpp-embed": mpp_embed
+        }
+
+    def gen_file_copy(self, content_id):
+        return {
+            "from": {
+                "mpp-format-string": "input://inlinefile"+ str(content_id) + "/{embedded['image_content_id_" + str(content_id) +"']}"
+            },
+            "to": "tree:///image_content_" + str(content_id)
+        }
+
+    def gen_file_copy_out(self, content_id, data):
+        return {
+            "from": "input://extra/image_content_" + str(content_id),
+            "to": "tree://" + data["path"]
+        }
+
+    def add_file_copy(self, contents, data):
+        content_id = self.gen_id()
+        self.file_content_inputs["inlinefile" + str(content_id)] = self.gen_file_input(content_id, data)
+        self.file_content_paths.append(self.gen_file_copy(content_id))
+        contents.file_content_copy.append(self.gen_file_copy_out(content_id, data))
+
+
+    def generate(self):
+        extra_include_pipelines = []
+        file_content_stages = []
+
+        if self.file_content_inputs:
+            file_content_stages.append({
+                "type": "org.osbuild.copy",
+                "inputs": self.file_content_inputs,
+                "options": {
+                    "paths": self.file_content_paths
+                }
+            })
+
+        if file_content_stages:
+            extra_include_pipelines.append({
+                "name": "extra-image-content",
+                "stages": file_content_stages
+        })
+
+        return {
+            "version": "2",
+            "pipelines": extra_include_pipelines
+        }
+
+# Both the rootfs and qm partition contents are specified with the same
+# syntax, but use slightly different mpp variables, to share this code
+# we have one class (Contents) for the rootfs one and override some details
+# in the QMContents class.
+class Contents:
+    def __init__(self, loader, data, extra_include):
+        self.loader = loader
+        self.extra_include = extra_include
+
+        self.repos = data.get("repos", [])
+        self.rpms = data.get("rpms", [])
+        self.containers = data.get("container_images", [])
+        self.add_files = data.get("add_files", [])
+        self.chown_files = data.get("chown_files", [])
+        self.chmod_files = data.get("chmod_files", [])
+        self.remove_files = data.get("remove_files", [])
+        self.make_dirs = data.get("make_dirs", [])
+        self.file_content_copy = []
+        self.systemd = data.get("systemd", {})
+
+    # Gets key to use for target partition (rootfs/qm)
+    def get_key(self, key):
+        return key
+
+    # Sets different key depending on target partition
+    def set_define(self, key, value):
+        key = self.get_key(key)
+        self.loader.set(key, value)
+
+    def set_defines(self):
+        for file in self.add_files:
+            self.extra_include.add_file_copy(self, file)
+        self.set_define("extra_copy", self.file_content_copy)
+
+        self.set_define("extra_mkdir", self.make_dirs)
+
+        chmod_files = {f["path"]: without(f, "path") for f in self.chmod_files}
+        self.set_define("extra_chmod", chmod_files)
+
+        chown_files = {f["path"]: without(f, "path") for f in self.chown_files}
+        self.set_define("extra_chown", chown_files)
+
+        extra_remove = []
+        for remove in self.remove_files:
+            extra_remove.append(remove["path"])
+        self.set_define("extra_remove", extra_remove)
+
+        for r in self.repos:
+            url = r["baseurl"]
+            r["baseurl"] = url.replace("$arch", self.loader.defines["arch"])
+
+        self.set_define("extra_repos", self.repos)
+        self.set_define("extra_rpms", self.rpms)
+
+        self.set_define("extra_containers", self.containers)
+        if self.containers:
+            self.set_define("use_containers_extra_store", True)
+
+        if self.systemd:
+            self.set_define("extra_systemd", self.systemd)
+
+class QMContents(Contents):
+    def __init__(self, loader, data, extra_include):
+        Contents.__init__(self, loader, data, extra_include)
+
+    def get_key(self, key):
+        if key.startswith("use_"):
+            return "use_qm_" + key[4:]
+        return "qm_" + key
+
+    def set_defines(self):
+        Contents.set_defines(self)
+
+
+class ManifestLoader():
+    def __init__(self, defines):
+        self.aib_basedir = defines["_basedir"]
+        self.workdir = defines["_workdir"]
+        self.defines = defines
+
+        # Note: Draft7 is what osbuild uses, and is available in rhel9
+        with open(os.path.join(self.aib_basedir, "files/manifest_schema.yml"), mode="r") as file:
+            self.aib_schema = yaml.load(file, yaml.SafeLoader)
+            jsonschema.Draft7Validator.check_schema(self.aib_schema)
+
+        self.validator = jsonschema.Draft7Validator(self.aib_schema)
+
+    def set(self, key, value):
+        self.defines[key] = value
+
+    def set_from(self, key, src_dict, src_key, default = None):
+        if src_key in src_dict:
+            self.set(key, src_dict[src_key])
+        elif default != None:
+            self.set(key, default)
+
+    def handle_qm(self, qm, extra_include):
+        self.set("use_qm", True)
+        if "content" in qm:
+            qm_content = QMContents(self, qm["content"], extra_include)
+            qm_content.set_defines()
+
+        memory_limit = qm.get("memory_limit", {})
+        self.set_from("qm_memory_max", memory_limit, "max")
+        self.set_from("qm_memory_high", memory_limit, "high")
+
+    def handle_network(self, network):
+        static = network.get("static", {})
+        if static:
+            self.set("use_static_ip", True)
+            self.set_from("static_ip", static, "ip")
+            ip_prefixlen = static["ip_prefixlen"]
+            if ip_prefixlen:
+                self.set("static_ip_prefixlen", str(ip_prefixlen))
+            self.set_from("static_gw", static, "gateway")
+            self.set_from("static_dns", static, "dns")
+            self.set_from("static_ip_iface", static, "iface")
+            self.set_from("static_ip_module", static, "load_module")
+
+    def handle_auth(self, auth):
+        # Always override to disable by default
+        self.set_from("root_password", auth, "root_password", "")
+        self.set_from("root_ssh_key", auth, "root_ssh_key")
+        self.set_from("extra_sshd_config", auth, "sshd_config")
+        self.set_from("extra_groups", auth, "groups")
+        self.set_from("extra_users", auth, "users")
+
+    def handle_kernel(self, kernel):
+        self.set_from("kernel_package", kernel, "kernel_package")
+        self.set_from("kernel_version", kernel, "kernel_version")
+        self.set_from("kernel_loglevel", kernel, "loglevel")
+        self.set_from("use_debug", kernel, "debug_logging")
+        self.set_from("extra_kernel_opts", kernel, "cmdline")
+
+    def handle_image(self, image):
+        image_size = image.get("image_size")
+        if image_size:
+            image_size = parse_size(image_size)
+            self.set("image_size", str(image_size))
+        partitions = image.get("partitions", {})
+        for k in partitions:
+            part = partitions[k]
+            if k == "var": # Separate /var partition
+                self.set("use_separate_var", True)
+                if "size" in part:
+                    var_size = part.get("size")
+                    var_size = parse_size(var_size)
+                    if image_size and var_size >= image_size:
+                        print("Error: /var can't be larger than image")
+                        sys.exit(1)
+                    self.set("varpart_size", var_size / 512)
+                else:
+                    rel_var_size = part.get("relative_size")
+                    if rel_var_size < 0 or rel_var_size >= 1:
+                        print("Error: Invalida relative var size, must be between 0 and 1")
+                        sys.exit(1)
+                    self.set("varpart_relative_size", rel_var_size)
+            else: # Non /var
+                if "size" in part:
+                    part_size = parse_size(part["size"])
+                    self.set(k + "part_size", int(part_size / 512))
+        self.set_from("hostname", image, "hostname")
+        self.set_from("osname", image, "osname")
+
+    def load(self, path, manifest_basedir):
+        with open(path, mode="r") as f:
+            try:
+                manifest = yaml.safe_load(f)
+            except yaml.YAMLError as exc:
+                raise exceptions.ManifestParseError(args.manifest) from exc
+
+        errors = sorted(self.validator.iter_errors(manifest), key=lambda e: e.path)
+        if errors:
+            raise exceptions.SimpleManifestParseError(path, errors)
+
+        # Extra include snippet for content, shared between contents
+        extra_include = ExtraInclude(manifest_basedir)
+
+        name = manifest["name"]
+        self.set("name", name)
+
+        content = Contents(self, manifest.get("content", {}), extra_include)
+        content.set_defines()
+
+        if "qm" in manifest:
+            self.handle_qm(manifest["qm"], extra_include)
+
+        self.handle_network(manifest.get("network", {}))
+        self.handle_auth(manifest.get("auth", {}))
+        self.handle_kernel(manifest.get("kernel", {}))
+        self.handle_image(manifest.get("image", {}))
+
+        # Write out extra_include mpp file for file content
+        extra_include_path = os.path.join(self.workdir, "extra-include.ipp.yml")
+        with open(extra_include_path, "w") as f:
+            yaml.dump(extra_include.generate(), f, sort_keys=False)
+        self.set("extra_import", extra_include_path)
